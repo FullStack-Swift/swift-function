24570f7e5d54af83ee08c59aa49b2a11f5ebcd1a
Nguyen Phong <nguyenphong.mobile.engineer@gmail.com>
2023-04-18 13:38:22 +0700

    Initial Commit
    Signed-off-by: Nguyen Phong <nguyenphong.mobile.engineer@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftFunction
+
+A description of this package.
---
+++ Tests/SwiftFunctionTests/SwiftFunctionTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import SwiftFunction
+
+final class SwiftFunctionTests: XCTestCase {
+  func testExample() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+  }
+}
